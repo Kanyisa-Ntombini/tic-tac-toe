3b925660db18fc413682096e53aec26329954ba7
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-10-10 19:00:31 +0200

    added player options

--- player-options/friend/js/friend-dom.js
+++ player-options/friend/js/friend-dom.js
@@ -1,31 +1,49 @@
 let player = Play();
 
 function nextFunc() {
-    document.querySelector('.error-messages').innerHTML = '';
+    /*document.querySelector('.error-messages').innerHTML = '';
+    document.querySelector('.turn').innerHTML = '';
 
     //HTML ELEMENTS
-    const box1 = document.getElementById('box1').value;
-    const box2 = document.getElementById('box2').value;
-    const box3 = document.getElementById('box3').value;
-    const box4 = document.getElementById('box4').value;
-    const box5 = document.getElementById('box5').value;
-    const box6 = document.getElementById('box6').value;
-    const box7 = document.getElementById('box7').value;
-    const box8 = document.getElementById('box8').value;
-    const box9 = document.getElementById('box9').value;
+    const box1 = document.querySelector('.box1').value;
+    const box2 = document.querySelector('.box2').value;
+    const box3 = document.querySelector('.box3').value;
+    const box4 = document.querySelector('.box4').value;
+    const box5 = document.querySelector('.box5').value;
+    const box6 = document.querySelector('.box6').value;
+    const box7 = document.querySelector('.box7').value;
+    const box8 = document.querySelector('.box8').value;
+    const box9 = document.querySelector('.box9').value;
 
-    const win = document.getElementById('win');
+    const win = document.querySelector('.win');
     const errorMessages = document.querySelector('.error-messages');
-    const turn = document.getElementById('turn');
+    const turn = document.querySelector('.turn');
 
-    //New variables for checked letters
+    //Check if X or O is inserted
 
     let b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    
 
-    if (player.checkLetterLength(box1) && player.checkLetterChar(box1)) {
+    if (box1=='x' || box1=='X' || box1=='o' || box1=='O') {
+        console.log('box 1 if');
+        b1 = box1.toLowerCase();
+    } else if (box2=='x' || box2=='X' || box2=='o' || box2=='O') {
+        console.log('box 2 if');
+        b2 = box2.toLowerCase();
+    } else if (box3=='x' || box3=='X' || box3=='o' || box3=='O') {
+        console.log('box 3 if');
+        b3 = box3.toLowerCase();
+    } else {
+        console.log('else');
+        document.querySelector('.error-messages').innerHTML = 'Please enter X or O';
+    }
+
+    /*if (player.checkLetterLength(box1) && player.checkLetterChar(box1)) {
         b1 = box1.toLowerCase();
     } else if (! player.checkLetterLength(box1) || ! player.checkLetterChar(box1)) {
         errorMessages.innerHTML = 'Please enter X or O';
+        console.log(box1);
+        console.log('else');
     }
 
     if (player.checkLetterLength(box2) && player.checkLetterChar(box2)) {
@@ -136,17 +154,16 @@ function nextFunc() {
     else if (b1=='o' || b2=='o' || b3=='o' || b4=='o' || b5=='o' || b6=='o' || b7=='o' || b8=='o' || b9=='o') {
         turn.classList.add('yellow');
         turn.innerHTML = 'Player X turn';
-    }
+    }*/
+    alert('next');
 }
 
-const nextBtn = document.querySelector('.nextBtn');
-nextBtn.addEventListener('click', nextFunc);
+/* Restart Function */
 
-/* Restart Button */
-
-const restartBtn = document.querySelector('.restartBtn');
 function restartFunc() {
     alert('Restart');
 }
 
-restartBtn.addEventListener('click', restartFunc);
+function func1() {
+    document.querySelector('.box1').innerHTML = 'X';
+}
